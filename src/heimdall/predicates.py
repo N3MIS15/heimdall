3a3811d3519e0dce2f769f43c635fd0c2ebb6df1
Garrett Brown <garbearucla@gmail.com>
2013-03-30 02:38:58 -0700

    Add the following predicates: dc:type, representing genres, and video:trailer for movie trailers

--- src/heimdall/predicates.py
+++ src/heimdall/predicates.py
@@ -13,13 +13,13 @@ def PredicateBuilder(short, namespace, properties):
 rdf = PredicateBuilder("rdf", "http://www.w3.org/TR/rdf-schema/", [ "Class" ])
 
 # Certain
-dc = PredicateBuilder("dc", "http://purl.org/dc/elements/1.1/", [ "title", "creator", "description", "format", "identifier", "date" ])
+dc = PredicateBuilder("dc", "http://purl.org/dc/elements/1.1/", [ "title", "creator", "description", "type", "format", "identifier", "date" ])
 owl = PredicateBuilder("owl", "http://www.w3.org/2002/07/owl#", [ "sameAs", "differentFrom" ])
 foaf = PredicateBuilder("foaf", "http://xmlns.com/foaf/spec/", [ "Agent", "Group", "Organization", "Person", "Document", "Image", "thumbnail", "homepage" ])
 
 # Uncertain
 media = PredicateBuilder("media", "http://purl.org/media#", [ "duration", "rating" ])
-video = PredicateBuilder("video", "http://purl.org/video#", [ ])
+video = PredicateBuilder("video", "http://purl.org/video#", [ "trailer" ])
 audio = PredicateBuilder("audio", "http://purl.org/audio#", [ ])
 
 upnp = PredicateBuilder("upnp", "urn:schemas-upnp-org:metadata-1-0/upnp/", [ "artist", "album", "originalTrackNumber" ])
